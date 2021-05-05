f0249c1fae215cdc02ec6d42d16d4256180ff259
Eric Berglund <ericberglund117@gmail.com>
2021-05-04 19:34:52 -0600

    Create logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import Logo from './logo/Logo.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Logo />
     </div>
   );
 }
