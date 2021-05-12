5c297a5d78e7d5818c457ccaaf047d5bb9e9dc89
Eric Berglund <ericberglund117@gmail.com>
2021-05-12 12:36:10 -0600

    Add snow to tall mountain

--- src/mainPage/MainPage.js
+++ src/mainPage/MainPage.js
@@ -3,11 +3,21 @@ import "./MainPage.css"
 function MainPage() {
   return(
     <div className="background">
-       <section className="mountain"></section>
+       <section className="mountain">
+           <section className="snow"></section>
+           <section className="snow-jagged"></section>
+           <section className="snow-jagged1"></section>
+       </section>
        <section className="mountain-1"></section>  
        <section className="mountain-2"></section> 
-       <section className="mountain-shadow"></section>  
-       <section className="mountain-shadow-1"></section>  
+       <section className="mountain-shadow">
+           <section className="snow-shadow"></section>
+           <section className="snow-shadow-jagged"></section>
+       </section>  
+       <section className="mountain-shadow-1">
+           <section className="snow-shadow1"></section>
+           <section className="snow-shadow-jagged1"></section>
+       </section>  
        <section className="mountain-shadow-2"></section>  
     </div>
   ) 
