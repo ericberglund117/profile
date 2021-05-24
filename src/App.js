cc7f43dcd88555c074a80cce7ef8589b0deef8af
Eric Berglund <ericberglund117@gmail.com>
2021-05-24 15:56:44 -0600

    Update styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './header/Header';
 import { StyledButton } from './styledComponents.styles';
 import './App.css';
 import MainPage from './mainPage/MainPage';
+import Design from './design/Design';
 import About from './about/About';
 import {Route, Switch, Link} from 'react-router-dom';
 
@@ -9,46 +10,20 @@ import {Route, Switch, Link} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <section className="header-section">
+      <section className="main-header-section">
         <Link to={`/`}>
           <img className="eb-logo" src="https://fontmeme.com/permalink/210505/81d25d957f62d7bbd5b434142331e1f7.png" alt="initial logo" />
         </Link>
         <Header />
       </section>
 
-        <Route path="/about">
-          <About />
-        </Route>
-
         <Route exact path="/">
-         <MainPage />
+         {/* <MainPage /> */}
+         {/* <Design /> */}
         </Route>
 
-        <section className="left-section">
-          <Link to={"/about"}>
-            <StyledButton>
-              <div id="translate"></div>
-                <h2 className="link" id="addListButton">About</h2>
-            </StyledButton>
-          </Link>
-        </section>
-
-        <section className="right-section">
-          <Link to={"/contact"}>
-            <StyledButton>
-              <div id="translate"></div>
-                <h2 className="link" id="addListButton">Contact</h2>
-            </StyledButton>
-          </Link>
-        </section>
-
-        <section className="middle-section">
-          <Link to={"/projects"}>
-            <StyledButton>
-              <div id="translate"></div>
-                <h2 className="link" id="addListButton">Projects</h2>
-            </StyledButton>
-          </Link> 
+        <section className="page">
+          <About />
         </section>
     </div>
   );
--- src/header/Header.js
+++ src/header/Header.js
@@ -1,5 +1,7 @@
 import Logo from '../logo/Logo.js'
 import "./Header.css"
+import {Route, Switch, Link} from 'react-router-dom';
+import { StyledButton } from '../styledComponents.styles';
 
 
 function Header() {
@@ -7,6 +9,25 @@ function Header() {
     <section className="header-section">
       <h1 className="name">Eric Berglund</h1>
       <h2 className="title">Frontend Software Engineer</h2>
+      <section className="buttons">
+            <section className="middle-section">
+              <Link to={"/contact"}>
+                <StyledButton>
+                  <div id="translate"></div>
+                    <h2 className="link" id="addListButton">Contact</h2>
+                </StyledButton>
+              </Link>
+            </section>
+
+            <section className="right-section">
+              <Link to={"/projects"}>
+                <StyledButton>
+                  <div id="translate"></div>
+                    <h2 className="link" id="addListButton">Projects</h2>
+                </StyledButton>
+              </Link> 
+            </section>
+          </section>
     </section>
   ) 
 } 
